3d8a4b6cdee6f5225ee9306f7f9ffd7f25d4216d
Мария <55990862+Mashaafanasowa@users.noreply.github.com>
2025-01-16 16:52:27 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username='Мария'
+title='Продукты'
+content='Молоко, Хлеб, Чай'
+status='Выполнено'
+created_date='10-01-2025'
+issue_date='10-02-2025'
+print("Имя пользователя-",username)
+print("Заголовок заметки-",title)
+print("Описание заметки:",content)
+print("Статус заметки-",status)
+print("Дата создания заметки-",created_date)
+print("Дата истечения заметки-",issue_date)
