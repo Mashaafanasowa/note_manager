7c35ce57307c44d3c54f1b52887d8c55119bce6c
Мария <55990862+Mashaafanasowa@users.noreply.github.com>
2025-01-17 18:22:45 +0300

    date_changer

---
+++ date_changer.py
@@ -0,0 +1,9 @@
+created_date='10-01-2025'
+issue_date='15 января 2025'
+temp_issue_date='10-01-2025'
+temp_created_date='15 января 2025'
+print("Оригинальная дата:",created_date)
+print("Преобразованная дата:",temp_issue_date[0:5])
+print("Оригинальная дата:",issue_date)
+print("Преобразованная дата:",temp_created_date[0:9])
+
